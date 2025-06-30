776c0d2dfdc8ec9ba44951a6bed70440178fa194
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-06-30 14:32:22 +0300

    add base structure

---
+++ eshop/Models/OrderComposition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.Models
+{
+    internal class OrderComposition
+    {
+        internal int id;
+        private int productId;
+        public int ProductId { get; set; }
+        private decimal price;
+        public decimal Price { get; set; }
+        private int quantity = 1;
+        public int Quantity
+        {
+            get => quantity;
+            set
+            {
+                quantity = value >= 1 ? value : 1;
+            }
+        }
+    }
+}
---
+++ eshop/Program.cs
@@ -0,0 +1,15 @@
+﻿using eshop.Models;
+namespace eshop
+{
+    internal class Program {
+        static void Main()
+        {
+            Book book1 = new Book("Грозовой перевал", 467.55m, "Эмили Бронте");
+            Product book4 = new Book("Грозовой перевал", 467.55m, "Эмили Бронте");
+            book1.Print();
+            book4.Print();
+            Console.WriteLine($"Книга 4: {book4}");
+            Console.WriteLine(book1);
+        }
+    };
+}
