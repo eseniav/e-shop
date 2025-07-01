e78dfea3aa1aaebf8232c6d7ffceb12dc9d952c4
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-07-01 17:57:22 +0300

    add 5 books

--- eshop/Models/Store.cs
+++ eshop/Models/Store.cs
@@ -8,7 +8,34 @@ namespace eshop.Models
 {
     internal class Store
     {
-        public List<Product> products = new List<Product>();
+        public List<Product> products = new List<Product>
+        {
+            new Book("Властелин Колец: Братство Кольца", 899m, "Дж. Р. Р. Толкин")
+                {
+                    BookGenre = Book.Genre.Фэнтези,
+                    PublicationDate = 1954
+                },
+            new Book("1984", 450m, "Джордж Оруэлл")
+                {
+                    BookGenre = Book.Genre.Антиутопия,
+                    PublicationDate = 1949
+                },
+            new Book("Убийство в \"Восточном экспрессе\"", 680m, "Агата Кристи")
+                {
+                    BookGenre = Book.Genre.Детектив,
+                    PublicationDate = 1934
+                },
+            new Book("Марсианин", 750m, "Энди Вейер")
+                {
+                    BookGenre = Book.Genre.НаучнаяФантастика,
+                    PublicationDate = 2011
+                },
+            new Book("Гарри Поттер и философский камень", 1200m, "Дж. К. Роулинг")
+                {
+                    BookGenre = Book.Genre.Фэнтези,
+                    PublicationDate = 1997
+                }
+        };
         public Product? GetProdById(int id) => products.Find(p => p.id == id);
         public List<Manufacturer> manufacturers = new() {
             new Manufacturer("АСТ", Country.Россия),
